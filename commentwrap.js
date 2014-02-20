865b945557cedf15ee0a5b15d34cae5b64d42ff2
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-20 22:12:21 +1100

    Reformat code to current style

--- commentwrap.js
+++ commentwrap.js
@@ -1,49 +1,51 @@
-var reLeadingDot = /^\./,
-    slashStarComments = {
-        leading: '/*',
-        line:    ' * ',
-        end:     ' */ '
-    },
-    fileComments = {
-        js: slashStarComments,
-        css: slashStarComments,
-        coffee: {
-            leading: '###',
-            line:    '',
-            end:     '###'
-        }
-    },
-    reLineBreak = /\r?\n/,
+var reLeadingDot = /^\./;
+var slashStarComments = {
+  leading: '/*',
+  line:    ' * ',
+  end:     ' */ '
+};
+var fileComments = {
+  js: slashStarComments,
+  css: slashStarComments,
+  coffee: {
+    leading: '###',
+    line:    '',
+    end:     '###'
+  }
+};
+var reLineBreak = /\r?\n/;
 
-    // intialise line endings based on platform
-    lineEnding = process.platform == 'win32' ? '\r\n' : '\n';
+// intialise line endings based on platform
+var lineEnding = process.platform == 'win32' ? '\r\n' : '\n';
 
 module.exports = function(opts, content) {
-    // split the content on line breaks
-    var lines = [''].concat((content || '').split(reLineBreak)).concat(''),
-        lastLineIdx = lines.length - 1,
-        filetype = (opts.filetype || 'js').replace(reLeadingDot, ''),
-        comments;
+  // split the content on line breaks
+  var lines = [''].concat((content || '').split(reLineBreak)).concat('');
+  var lastLineIdx = lines.length - 1;
+  var filetype = (opts.filetype || 'js').replace(reLeadingDot, '');
+  var comments;
 
-    // get the comments for the specific filetype
-    comments = fileComments[filetype] || slashStarComments;
+  // get the comments for the specific filetype
+  comments = fileComments[filetype] || slashStarComments;
+  
+  // if we have a leader then add an additional line for a tidy output
+  if (opts.leader) {
+    lines.unshift('');
+  }
+  
+  // iterate through the lines and add the comments
+  lines = lines.map(function(line, index) {
+    var comment = comments[index === 0 ? 'leading' : 'line'];
     
-    // if we have a leader then add an additional line for a tidy output
-    if (opts.leader) { lines.unshift(''); }
-    
-    // iterate through the lines and add the comments
-    lines = lines.map(function(line, index) {
-        var comment = comments[index === 0 ? 'leading' : 'line'];
-        
-        // if we are at the first line, then add the optional leader
-        if (index === 0) {
-            comment += (opts.leader || '');
-        }
+    // if we are at the first line, then add the optional leader
+    if (index === 0) {
+      comment += (opts.leader || '');
+    }
 
-        // add the comment line to the content line
-        return comment + line;
-    }).concat(comments.end);
+    // add the comment line to the content line
+    return comment + line;
+  }).concat(comments.end);
 
-    // wrap in the comments
-    return lines.join(lineEnding);
+  // wrap in the comments
+  return lines.join(lineEnding);
 };
--- index.js
+++ index.js
@@ -1,64 +1,103 @@
-var async = require('async'),
-    fs = require('fs'),
-    path = require('path'),
-    _existsSync = fs.existsSync || path.existsSync,
-    commentwrap = require('./commentwrap'),
-    _ = require('underscore'),
-    defaultData = {
-        name: '',
-        description: '',
-        generator: 'headerblock',
-        url: null,
-        builddate: new Date().toISOString(),
-        md5: null,
-        copyright: null,
-        licenses: []
-    };
+/* jshint node: true */
+
+/**
+  # headerblock
+
+  This is a small helper module that is used within
+  [Interleave](https://github.com/buildjs/interleave) to generate header
+  block comments for source files.
+
+  ## Example Usage
+
+  ```js
+  var headerblock = require('headerblock');
+  var packageData = require('./package.json');
+
+  // call headerblock with artificial data
+  headerblock(packageData, function(err, output) {
+    // generated, js block commented header generated and in output
+  });
+  ```
+
+  If you want to generate a header block for a language such as
+  [CoffeeScript](https://coffee-script.org) then you can specify the filetype:
+
+  ```js
+  var headerblock = require('headerblock');
+  var packageData = require('./package.json');
+
+  // call headerblock with artificial data
+  headerblock(packageData, { filetype: 'coffee' }, function(err, output) {
+    // generated, coffeescript block commented header generated and in output
+  });
+  ```
+
+  ## Sample Generated Header Blocks
+
+  To be completed.
+**/
+
+var async = require('async');
+var fs = require('fs');
+var path = require('path');
+var _existsSync = fs.existsSync || path.existsSync;
+var commentwrap = require('./commentwrap');
+var _ = require('underscore');
+var defaultData = {
+  name: '',
+  description: '',
+  generator: 'headerblock',
+  url: null,
+  builddate: new Date().toISOString(),
+  md5: null,
+  copyright: null,
+  licenses: []
+  };
 
 module.exports = function(data, opts, callback) {
-    var templatePaths = [ path.resolve(__dirname, 'templates', 'default.txt') ],
-        output;
-    
-    // handle the 2 args case
-    if (typeof opts == 'function') {
-        callback = opts;
-        opts = {};
-    }
-    
-    // ensure we have opts
-    opts = opts || {};
-    
-    // ensure the data has defaults
-    data = _.defaults({}, data, defaultData);
+  var templatePaths = [ path.resolve(__dirname, 'templates', 'default.txt') ];
+  var output;
+  
+  // handle the 2 args case
+  if (typeof opts == 'function') {
+    callback = opts;
+    opts = {};
+  }
+  
+  // ensure we have opts
+  opts = opts || {};
+  
+  // ensure the data has defaults
+  data = _.defaults({}, data, defaultData);
 
-    // initialise the default version (using this technique to get around
-    // versionit being clever)
-    data.version = data.version || '';
-    
-    // if we have a template option specified, then add some additional paths
-    if (opts.template) {
-        templatePaths.shift(path.resolve(__dirname, 'templates', opts.template + '.txt'));
-        templatePaths.shift(path.resolve(opts.template));
-    }
-    
-    // find the first valid template
-    async.detect(templatePaths, fs.exists || path.exists, function(templatePath) {
-        // read the template file
-        fs.readFile(templatePath, 'utf8', function(err, content) {
-            if (err) return callback(err);
-            
-            // add the packagename leader if it doesn't exist already
-            if (data.name) {
-                opts.leader = ' ~' + data.name + '~';
-            }
-            
-            try {
-                // fire the callback
-                return callback(null, commentwrap(opts, _.template(content, _.defaults({}, data, opts))));
-            }
-            catch (e) {
-                return callback(e);
-            }
-        });
+  // initialise the default version (using this technique to get around
+  // versionit being clever)
+  data.version = data.version || '';
+  
+  // if we have a template option specified, then add some additional paths
+  if (opts.template) {
+    templatePaths.shift(path.resolve(__dirname, 'templates', opts.template + '.txt'));
+    templatePaths.shift(path.resolve(opts.template));
+  }
+  
+  // find the first valid template
+  async.detect(templatePaths, fs.exists || path.exists, function(templatePath) {
+    // read the template file
+    fs.readFile(templatePath, 'utf8', function(err, content) {
+      if (err) return callback(err);
+      
+      // add the packagename leader if it doesn't exist already
+      if (data.name) {
+        opts.leader = ' ~' + data.name + '~';
+      }
+      
+      try {
+        // fire the callback
+        return callback(null, commentwrap(opts, _.template(content, _.defaults({}, data, opts))));
+      }
+      catch (e) {
+        return callback(e);
+      }
     });
+  });
 };
